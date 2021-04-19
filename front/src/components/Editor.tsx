22e1d6c6836033b99109f2f6e8ea59690bf8efd3
Jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2021-04-19 21:35:21 +0200

    feat: added checking result

--- front/src/components/Editor.tsx
+++ front/src/components/Editor.tsx
@@ -1,22 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AceEditor from 'react-ace'
 
 import "ace-builds/src-noconflict/mode-javascript";
 import "ace-builds/src-noconflict/theme-github";
 import "ace-builds/src-noconflict/keybinding-vim"
 
-const Editor = () => {
+interface Props {
+	writeable: boolean;
+	code: string;
+	setCode: (value: string) => void;
+}
+
+const Editor: React.FC<Props> = ({ writeable, code, setCode }) => {
+	const handleChange = (value: string) => {
+		setCode(value)
+	}
+
 	return (
-		<div className="editor">
+		<div className={`editor ${!writeable ? "ignore-pointer-events" : ""}`}>
 			<AceEditor
 				placeholder="Placeholder Text"
 				mode="javascript"
 				theme="github"
 				name="hola"
+				onChange={handleChange}
+				focus={writeable}
+				readOnly={!writeable}
 				highlightActiveLine={true}
-				value={`function onLoad(editor) {
-  console.log("i've loaded");
-}`}
+				value={code}
 				width={`${window.innerWidth / 2}px`}
 				height={`${window.innerHeight / 2}px`}
 				setOptions={{
--- front/src/components/Level.tsx
+++ front/src/components/Level.tsx
@@ -1,10 +1,62 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { LevelContext } from '../contexts/contexts'
 import Editor from './Editor'
 
 const Level = () => {
+	const [keyCount, setKeyCount] = useState(0)
+	const { level } = useContext(LevelContext)
+	const [code, setCode] = useState(level.start_code)
+	const [codeIsWrong, setCodeIsWrong] = useState<boolean | null>(null)
+
+	const increaseKeyCount = () => setKeyCount(prevCount => prevCount + 1)
+
+	useEffect(() => {
+		window.addEventListener("keydown", increaseKeyCount)
+
+		return () => {
+			window.removeEventListener("keydown", increaseKeyCount)
+		}
+	}, [])
+
+	const check = () => {
+		let isCodeWrong
+		if (code === level.end_code) {
+			isCodeWrong = false
+		} else {
+			isCodeWrong = true
+		}
+		setCodeIsWrong(isCodeWrong)
+	}
+
 	return (
-		<div className="level flex align-center jusitfy-center">
-			<Editor />
+		<div className="level flex flex-column align-center jusitfy-center">
+			<div className="editors">
+				<div>
+					<h1>Editor</h1>
+					<Editor writeable={true} code={code} setCode={setCode} />
+				</div>
+				<div>
+					<h1>Result</h1>
+					<Editor writeable={false} code={level.end_code} setCode={setCode} />
+				</div>
+			</div>
+			<h2> {level.creator} </h2>
+			<h2> Key count: {keyCount} </h2>
+			<button className="check" onClick={check}>Check</button>
+			{
+				codeIsWrong === true ?
+					<div className="note error">
+						Something is wrong. Keep trying.
+					</div>
+					:
+					codeIsWrong === null ?
+						<>
+						</>
+						:
+						<div className="note correct">
+							Good job
+						</div>
+			}
 		</div>
 	)
 }
